803995d365bb273972f19d20fdc9f5dcc35de833
onyekachila <onyekachila@gmail.com>
2019-11-28 12:36:43 +0100

    Intial Create

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EtcLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EtcLibrary.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository)); 
+        }
+
+        
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            //return new JsonResult(authorsFromRepo); 
+            return Ok(authorsFromRepo); 
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthorById(Guid authorId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId)) 
+            {
+                return NotFound(); 
+            }
+            var authorById = _courseLibraryRepository.GetAuthor(authorId);
+            return Ok(authorById); 
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,57 @@
+using EtcLibrary.API.DbContexts;
+using EtcLibrary.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EtcLibrary.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+
+            }).AddXmlDataContractSerializerFormatters();
+
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<EtcContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=.;Database=CourseLibraryDB;Trusted_Connection=True;");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
